e79999c1744fd3bebd5b40754f31da148f92b22d
joydpaul <joydeep@thenoiseinside.com>
2018-01-31 11:50:49 +0530

    regular update

---
+++ main.bundle.js
@@ -0,0 +1,152 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_gendir lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	return new Promise(function(resolve, reject) { reject(new Error("Cannot find module '" + req + "'.")); });
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n<div class=\"menu-section\">\n  <div class=\"menu-left-section  mobile-no-show\">\n      <a id=\"home\" href=\"/\"><img src=\"assets/images/tni-small-logo.png\" alt=\"tni small logo\" class=\"tni-nav-logo\"></a>\n      <ul class=\"menu-items\">\n          <li data-menuanchor=\"services-section\"><a class=\"menu-items-a\" href=\"#services-section\">offerings</a></li>\n          <li data-menuanchor=\"work-section\"><a class=\"menu-items-a\" href=\"#work-section\">work</a></li>\n          <li><a class=\"menu-items-a\" href=\"http://thenoiseinside.com/blog\" target=\"_blank\">blog</a></li>\n          <li data-menuanchor=\"contact-section\"><a class=\"menu-items-a\" href=\"#contact-section\">contact</a></li>\n      </ul>\n  </div>\n  <div class=\"menu-right-section\">\n      <ul class=\"menu-items-right\">\n          <!--<li class=\"mr-90 mobile-no-show\">agency</li>-->\n          <li class=\"noise-voice-switch\">\n              <div class=\"switch\">\n                <span class=\"switch-text\" id=\"switchtext\">NOISE</span>\n                <input id=\"cmn-toggle-1\" class=\"cmn-toggle cmn-toggle-round\" type=\"checkbox\" data-on-text=\"Credit\" data-on-color=\"success\" data-off-text=\"Cash\" data-off-color=\"warning\" tabindex=\"13\">\n                <label for=\"cmn-toggle-1\"></label>\n              </div>\n          </li>\n      </ul>\n  </div>\n</div>\n\n<div class=\"mobile-logo mobile-only\">\n  <a href=\"index.html\"><img src=\"assets/images/tni-small-logo.png\" alt=\"tni small logo\" class=\"tni-nav-logo\"></a>\n</div>\n\n<div>\n<ul id=\"menu\">\n<li data-menuanchor=\"services-section\"><a href=\"#services-section\">offerings</a></li>\n<li data-menuanchor=\"work-section\"><a href=\"#work-section\">work</a></li>\n  <li><a href=\"http://thenoiseinside.com/blog\" target=\"_blank\">blog</a></li>\n<li data-menuanchor=\"contact-section\"><a href=\"#contact-section\">contact us</a></li>\n</ul>\n</div>\n\n\n\n<section class=\"tni-home\">\n  <div class=\"tni-leftfixed-border\" id=\"tnileftfixedborder\">\n      <img src=\"assets/images/left-border.png\" alt=\"left border\" class=\"lt-border\" id=\"leftborder\">\n  </div>\n  <div class=\"tni-rightfixed-border\">\n      <!--<img src=\"images/right-border.png\" class=\"rt-border\">-->\n      <div class=\"tni-right-border\">\n              <img src=\"assets/images/right-top-border.png\" alt=\"right top border\" class=\"rt-border\">\n              <div class=\"right-social-icons\">\n                  <a href=\"https://www.facebook.com/TheNoiseIndia/\" target=\"_blank\"><img src=\"assets/images/facebook-icon.png\" alt=\"facebook icon\" class=\"social-icon\"></a><br>\n                  <a href=\"https://twitter.com/TheNoiseIndia\" target=\"_blank\"><img src=\"assets/images/twitter-icon.png\" alt=\"twitter icon\" class=\"social-icon\"></a><br>\n                  <a href=\"https://www.linkedin.com/company/the-noise-inside\" target=\"_blank\"><img src=\"assets/images/linked-in-icon.png\" alt=\"linkedin icon\" class=\"social-icon\"></a><br>\n                  <a href=\"https://www.instagram.com/effinoise/\" target=\"_blank\"><img src=\"assets/images/insta-icon.png\" alt=\"instagram icon\" class=\"social-icon\"></a>\n              </div>\n              <img src=\"assets/images/right-bottom-border.png\" alt=\"right bottom border\" class=\"rb-border\">\n      </div>\n  </div>\n</section>\n\n<div id=\"fullpage\">\n  <div class=\"section tnihome-section\">\n      <div class=\"tni-left-section\">\n      <div class=\"home-banner-img\">\n          <img class=\"home-img-border js-tilt\" alt=\"image border\" src=\"assets/images/home-border.png\">\n          <img class=\"home-img\" src=\"assets/images/home-img.png\" alt=\"home image\" id=\"homeimg\">\n          <img class=\"home-img\" src=\"assets/images/home-color-image.png\" alt=\"home color image\" id=\"homecolorimg\">\n      </div>\n      </div>\n      <div class=\"tni-right-section mobile-no-show\">\n          <img src=\"assets/images/tni-logo-cropped.png\" alt=\"image\" class=\"tni-logo\" id=\"tnilogo\">\n          <h1 class=\"home-main-text logo-text\">a creative digital agency</h1>\n          <p class=\"home-main-text\">With Design, Tech &amp; Marketing as our pillars, we assist and facilitate by crafting experiences, platforms and products that transform <span class=\"text-uppercase\">brands</span>.</p>\n          <img src=\"assets/images/home-horizontal-border.png\" alt=\"image\" class=\"home-horizontal-border\">\n          <p class=\"home-quote\">“If you want something, you better make some Noise”<br>- Malcolm X</p>\n      </div>\n  </div>\n\n  <div class=\"section tniservices-section\">\n      <h2 class=\"services-section-text service-head\">Design, Technology & Marketing.</h2>\n      <div class=\"services-box\">\n          <img src=\"assets/images/services-box.png\" alt=\"services box\" class=\"servicesbox-img js-tilt\">\n          <a id=\"design-link\" href=\"services.html#design-section\"><img id=\"pencil\" src=\"assets/images/design-pencil.png\" alt=\"design pencil\" class=\"services-pencil-img js-tilt\">\n          <img id=\"design\" src=\"assets/images/design-bubble-white.png\" alt=\"design-bubble-white\" class=\"design-bubble\"></a>\n          <a id=\"tech-link\" href=\"services.html#tech-section\"><img id=\"spacebar\" src=\"assets/images/tech-spacebar.png\" alt=\"tech-spacebar\" class=\"spacebar-img js-tilt\">\n          <img id=\"tech\" src=\"assets/images/tech-bubble-white.png\" alt=\"tech-bubble-white\" class=\"tech-bubble\"></a>\n          <a id=\"marketing-link\" href=\"services.html#marketing-section\"><img id=\"dart\" src=\"assets/images/marketing-dart.png\" alt=\"marketing-dart\" class=\"marketing-img js-tilt\">\n          <img id=\"marketing\" src=\"assets/images/marketing-bubble-white.png\" alt=\"marketing-bubble-white\" class=\"marketing-bubble\"></a>\n      </div>\n      <p class=\"services-section-below-text\"> Craft to impress and enhance experiences.<br>Transform your digital space with expertise.<br> Precision and elegance in execution, ensuring values. </p>\n  </div>\n\n  <div class=\"section\">\n      <section  class=\"portfolio-grid\">\n      <div>\n    <!-- Top Navigation -->\n    <ul class=\"grid effect-1\" id=\"grid\">\n      <li><a href=\"#\"><img src=\"assets/images/Sl.jpg\" alt=\"Sl\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/Open.jpg\" alt=\"Open\"></a></li>\n              <li><a href=\"#\"><img src=\"assets/images/FC.jpg\" alt=\"FC\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/Inbuzz.jpg\" alt=\"Inbuzz\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/Flo.jpg\" alt=\"Flo\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/WE.jpg\" alt=\"WE\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/Gb.jpg\" alt=\"GB\"></a></li>\n              <li><a href=\"#\"><img src=\"assets/images/Noc.jpg\" alt=\"Noc\"></a></li>\n      <li><a href=\"#\"><img src=\"assets/images/Hunger.jpg\" alt=\"Hunger\"></a></li>                \n              <li><p class=\"and-more\">&amp; More</p></li>\n          </ul>\n      </div>\n      </section>\n  </div>\n\n  <div class=\"section\">\n      <div class=\"contact-section\">\n          <p class=\"sounds-good\">Sounds Good?</p>\n          <p class=\"contact-text\">The best way to get in touch with <b>The Noise Inside - Creative Digital Agency</b> is by writing an <a href=\"mailto:hello@thenoiseinside.com\"><b><span class=\"underline\">email</span></b></a>, giving a call or simply coming by and saying hi. We’re looking forward to hearing from you. </p>\n\n          <div class=\"contact-form\">\n             <form name=\"tni-contact-form\" class=\"tni-contact\" action=\"contact-us.php\" method=\"post\">\n                  <div class=\"contactform-left\">\n                      <label for=\"name\">NAME</label><br>\n                      <input type=\"text\" name=\"name\">\n                      <label for=\"phone\">PHONE NUMBER</label><br>\n                      <input type=\"tel\" name=\"phone\">\n                      <label for=\"email\">EMAIL</label><br>\n                      <input type=\"email\" name=\"email\">\n                      <div id=\"error-container\"></div>\n                  </div>\n                  <div class=\"contactform-right\">\n                      <label for=\"message\">MESSAGE</label><br>\n                      <textarea name=\"message\"></textarea>\n                      <button class=\"tni-submit\" type=\"submit\">SEND</button>\n                  </div>\n                  <div class=\"clear-style\"></div>\n            </form>\n          </div>\n      </div>\n  </div>\n</div>\n\n<div id=\"noise-trans\"></div>\n<div id=\"voice-trans\"></div>\n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+var environment = {
+    production: true
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_19" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
